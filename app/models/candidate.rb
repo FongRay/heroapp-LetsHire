f9c8b63f3ee48e3b20bacb475e0c8f736f0e5b92
Yuan Zhuang <yuanzhuang@vmware.com>
2013-06-08 16:04:21 +0800

    for candidate scopes implemented by sql, replace them using AR pattern

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -31,33 +31,43 @@ class Candidate < ActiveRecord::Base
 
   scope :active, where(:status => NORMAL)
   scope :inactive, where(:status => INACTIVE)
-  scope :no_openings, where('id NOT IN (
+
+  scope :no_openings_sql, where('id NOT IN (
                             SELECT DISTINCT "opening_candidates"."candidate_id" FROM "opening_candidates"
                             INNER JOIN "openings" ON "opening_candidates"."opening_id" = "openings"."id"
                             WHERE "openings"."status" = 1 )')
+  scope :no_openings, lambda { where('id NOT IN (?)', OpeningCandidate.joins(:opening).where(:openings => {:status => 1}).map(&:candidate_id).join(',')) }
+
   scope :not_in_opening, ->(opening_id) { where("id NOT IN (SELECT DISTINCT candidate_id FROM opening_candidates WHERE opening_id=#{opening_id})") }
   scope :available, where(:status => INACTIVE).no_openings
   scope :with_opening, joins(:opening_candidates => :opening).where(:openings => {:status => 1}).uniq
 
-  scope :with_interview, where('current_opening_candidate_id IN (
+  scope :with_interview_sql, where('current_opening_candidate_id IN (
                                SELECT "opening_candidates"."id" FROM "opening_candidates"
                                INNER JOIN "interviews" ON "interviews"."opening_candidate_id" = "opening_candidates"."id")')
+  scope :with_interview, where(:current_opening_candidate_id => OpeningCandidate.joins(:interviews).pluck(:opening_candidate_id).uniq)
+
   #Todo: need filter out candidates not assigned to interviews
-  scope :no_interviews, where('id NOT in (
+  scope :no_interviews_sql, where('id NOT in (
                               SELECT DISTINCT "candidates"."id" FROM "candidates"
                               INNER JOIN "opening_candidates" ON "opening_candidates"."candidate_id" = "candidates"."id"
                               INNER JOIN "interviews" ON "interviews"."opening_candidate_id" = "opening_candidates"."id" )
                               AND current_opening_candidate_id > 0')
+  scope :no_interviews, lambda { where('id NOT IN (?)', Candidate.joins(:opening_candidates => :interviews).map(&:id).join(',')).where('current_opening_candidate_id > 0') }
 
-  scope :with_assessment, where('current_opening_candidate_id IN (
+  scope :with_assessment_sql, where('current_opening_candidate_id IN (
                                 SELECT "opening_candidates"."id" FROM "opening_candidates"
                                 INNER JOIN "assessments" ON "assessments"."opening_candidate_id" = "opening_candidates"."id"
                                 WHERE "assessments"."comment" IS NOT NULL)')
-  scope :without_assessment, where('current_opening_candidate_id NOT IN (
+  scope :with_assessment, where(:current_opening_candidate_id => OpeningCandidate.joins(:assessment).where('"assessments"."comment" IS NOT NULL'))
+
+  scope :without_assessment_sql, where('current_opening_candidate_id NOT IN (
                                    SELECT "opening_candidates"."id" FROM "opening_candidates"
                                    INNER JOIN "assessments" ON "assessments"."opening_candidate_id" = "opening_candidates"."id"
                                    WHERE "assessments"."comment" IS NOT NULL)
                                    AND current_opening_candidate_id > 0')
+  scope :without_assessment, lambda { where('current_opening_candidate_id NOT IN (?) AND current_opening_candidate_id > 0',
+                                   OpeningCandidate.joins(:assessment).where('"assessments"."comment" IS NOT NULL').map(&:id).uniq.join(',')) }
 
   def opening(index)
     opening_candidates[index].opening if opening_candidates.size > index
