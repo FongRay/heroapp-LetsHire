0d91927d107efe1c536f6cd42d6ad57b575370a2
Chunjie <czhu@vmware.com>
2013-06-13 19:12:29 +0800

    remove departments UT case

--- spec/views/departments/index.html.slim_spec.rb
+++ spec/views/departments/index.html.slim_spec.rb
@@ -8,8 +8,8 @@ describe "departments/index" do
     ])
   end
 
-  it "renders a list of departments" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-  end
+  #it "renders a list of departments" do
+  #  render
+  #  # Run the generator again with the --webrat flag if you want to use webrat matchers
+  #end
 end
